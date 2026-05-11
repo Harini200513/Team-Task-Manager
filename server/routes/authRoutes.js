0cb72f540f9afe9d65cc0a1557b47a1c67fc3ba8
Harini200513 <164490809+Harini200513@users.noreply.github.com>
2026-05-11 23:51:17 +0530

    Add resend OTP route to authRoutes

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { loginUser, registerUser, logoutUser, forgotPassword, resetPassword, verifyOTP } from '../controllers/authController.js';
+import { loginUser, registerUser, logoutUser, forgotPassword, resetPassword, verifyOTP, resendOTP } from '../controllers/authController.js';
 
 const router = express.Router();
 
@@ -9,5 +9,6 @@ router.post('/logout', logoutUser);
 router.post('/forgotpassword', forgotPassword);
 router.put('/resetpassword/:resettoken', resetPassword);
 router.post('/verify-otp', verifyOTP);
+router.post('/resend-otp', resendOTP);
 
 export default router;
